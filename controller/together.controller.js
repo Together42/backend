dcca4f036d9af5caa46b331f0acb0fd036b71b8b
TAEHO KIM <39645477+kth2624@users.noreply.github.com>
2022-04-19 01:55:02 +0900

    친바하기 생성 api 완료
    - 생성 api 완료

--- controller/together.controller.js
+++ controller/together.controller.js
@@ -1,4 +1,4 @@
-
+import * as togetherRepository from '../data/together.js';
 ///*
 //POST /api/together/matching
 //{
@@ -19,3 +19,29 @@ export async function team(req, res, next) {
 
 	res.status(201).json({test:req.body});
 }
+
+export async function createTogether(req, res, next) {
+	const { title, description } = req.body;
+	const together = await togetherRepository.createTogether({
+		title,
+		description,
+	});
+	//console.log(together);
+	res.status(201).json({together});
+}
+
+//export async function deleteTogether(req, res, next){
+//	const id = req.params.id;
+//	console.log(id);
+//	const deleteId = await togetherRepository.findByTogetherId(id);
+//	if(!deleteId) //삭제할 친바가 없다면
+//		return res.sendStatus(404);
+	
+//	await togetherRepository.deleteTogether(id);
+//	res.sendStatus(204);
+//}
+
+//export async function getTogethers(req, res, next){
+//	const togethers = await togetherRepository.getTogethers();
+//	res.status(200).json({togethers});
+//}
