74ec872121cdbb490a95d40384ccc41c01fb22f3
TAEHO KIM <39645477+kth2624@users.noreply.github.com>
2022-04-25 22:41:04 +0900

    ejs 부분 삭제

--- app.js
+++ app.js
@@ -1,35 +1,20 @@
 import express from 'express';
 import morgan from 'morgan';
-//const path = require("path");
-import path from 'path';
-import indexRouter from './routes/render/index.js';
-//const indexRouter = require('./api/routes/render/index');
 import { config } from './config.js';
 import authRouter from './routes/auth.js';
 import togetherRouter from './routes/together.js';
 
 // express configuration
 const app = express();
-const __dirname = path.resolve();
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-app.set('views', path.join(__dirname,'views'));
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
 
 //parse JSON and url-encoded query
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-//print the request log on console
 app.use(morgan('dev'));
-
-//set the secret key variable for jwt
-app.use('/', indexRouter);
 
 app.use('/api/auth', authRouter);
 app.use('/api/together', togetherRouter);
-
 
 app.listen(config.host.port);
 console.log("Listening on", config.host.port);
