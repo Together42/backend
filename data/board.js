a00ff67e1c539fc9774b91580550b3a2fe657307
TAEHO KIM <39645477+kth2624@users.noreply.github.com>
2022-05-05 04:26:15 +0900

    게시판 임시

---
+++ data/board.js
@@ -0,0 +1,84 @@
+import { db } from '../db/database.js';
+
+export async function getEventList(){
+	return db
+	.execute('SELECT ev.id, ev.title, ev.description at.teamId FROM event_info as ev JOIN attendance_info as at ON ev.id=at.id ')
+	.then((result)=>console.log(result[0]));
+}
+
+export async function findByEventId(id) {
+	return db
+	.execute('SELECT * FROM event_info WHERE id=?',[id])
+	.then((result) => result[0][0]);
+}
+
+export async function deleteEvent(id) {
+	return db.execute('DELETE FROM event_info WHERE id=?',[id]);
+}
+
+export async function createEvent(event) {
+	const {title, description, createdBy} = event;
+	return db
+	.execute('INSERT INTO event_info (title, description, createdBy) VALUES (?,?,?)',
+	[title, description, createdBy]
+	)
+	.then((result) => result[0].insertId);
+}
+
+export async function register(user, eventId) {
+	return db
+	.execute('INSERT INTO attendance_info (userId, eventId) VALUES (?,?)',
+	[user, eventId]
+	)
+	.then((result)=> result[0].insertId);
+}
+
+export async function unregister(user, eventId) {
+	return db
+	.execute('DELETE FROM attendance_info WHERE userId=? && eventId=?',
+	[user, eventId]
+	)
+}
+
+export async function findByAttend(user, eventId) {
+	return db
+	.execute('SELECT * FROM attendance_info WHERE userId=? && eventId=?',
+	[user, eventId]
+	)
+	.then((result) => result[0][0]);
+}
+
+export async function findAttendByEventId(eventId) {
+	return db
+	.execute('SELECT * FROM attendance_info WHERE eventId=?',
+	[eventId]
+	)
+	.then((result) => result[0]);
+}
+
+export async function getMatchingList(id)
+{
+	return db
+	.execute('SELECT us.loginId, us.url, at.teamId from attendance_info as at JOIN users as us ON at.userId=us.id WHERE at.eventId=? ORDER BY at.teamId',
+	[id]
+	)
+	.then((result) => result[0]);
+}
+
+
+export async function getByAttendId(id)
+{
+	return db
+	.execute('SELECT * FROM attendance_info WHERE id=?',
+	[id]
+	)
+	.then((result)=> result[0][0]);
+}
+
+export async function createTeam(teamId, id)
+{
+	return db
+	.execute('UPDATE attendance_info SET teamId=? WHERE id=?',
+	[teamId, id])
+	.then(()=> getByAttendId(id));
+}
