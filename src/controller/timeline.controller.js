22e2624496a76c439f0932c4f900742cc6dbdf78
sideseal <ckgun@naver.com>
2022-12-29 02:39:14 +0900

    fix: timeline api 이미지 변경 및 테스트 DB에 사진 추가

--- src/controller/timeline.controller.js
+++ src/controller/timeline.controller.js
@@ -1,20 +1,19 @@
 import * as timelineRepository from '../data/timeline.js'
-import {s3} from '../s3.js'
 
-export async function getTimelineImage(req, res) {
-  const fileName = req.params.filename
-  console.log(fileName)
-  if(!fileName)
-    return res.status(400).json({message: '조회할 이미지 파일이 없습니다'})
+export async function listAllImages(req, res) {
   try {
-    const image = await timelineRepository.findByImageFileName(fileName)
-    if (!image) {
-      return res.status(400).json({message: '찾고자 하는 파일이 없습니다'})
+    const images = await timelineRepository.listAllImages()
+    if (!images) {
+      return res.status(400).json({message: '이미지 조회 실패'})
     }
-    const imageUrl = image.filePath
-    console.log(imageUrl)
-    return res.status(200).json({url: imageUrl})
+    const imageUrl = images.map(item => {
+      return {
+        url: item.filePath,
+      }
+    })
+    return res.status(200).json({img: imageUrl})
   } catch (error) {
+    console.log(error)
     return res.status(400).json({message: '이미지 조회 실패'})
   }
 }
