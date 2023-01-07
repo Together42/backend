cea916927d4c5bec72d23adb25cf6e704f487f97
sideseal <ckgun@naver.com>
2023-01-07 17:23:58 +0900

    modify : 남아있는 사용하지 않는 기능 삭제 - User API

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ import authRouter from "./auth.routes.js";
 import togetherRouter from "./together.routes.js";
 import boardRouter from "./board.routes.js";
 import slackRouter from "./slack.routes.js";
-import userRouter from "./user.routes.js";
 import timelineRouter from "./timeline.routes.js";
 
 const router = Router();
@@ -12,7 +11,6 @@ router.use("/auth", authRouter);
 router.use("/together", togetherRouter);
 router.use("/board", boardRouter);
 router.use("/slack", slackRouter);
-router.use("/user", userRouter);
 router.use("/timeline", timelineRouter);
 
 export default router;
