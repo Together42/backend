0f9a8089ac76a2475efac7fc302b2299ad845547
sideseal <ckgun@naver.com>
2023-03-16 18:16:21 +0900

    added: add getHolidayOfMonth function

--- src/utils/rotation.calendar.js
+++ src/utils/rotation.calendar.js
@@ -1,3 +1,6 @@
+import request from "request";
+import { config } from "../config.js";
+
 export function getFourthWeekdaysOfMonth() {
   const today = new Date();
   const year = today.getFullYear();
@@ -15,6 +18,38 @@ export function getFourthWeekdaysOfMonth() {
       fourthWeekDays.push(day);
     }
   }
-
   return (fourthWeekDays);
+}
+
+export async function getHolidayOfMonth() {
+  const URL = 'http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getHoliDeInfo';
+  const solYear = new Date().getFullYear();
+  const month = new Date().getMonth() + 1;
+  const solMonth = month < 10 ? '0' + month : month;
+  const SERVICEKEY = config.openApi.holidayKey;
+  const requestUrl = URL + '?' + 'solYear=' + solYear + '&' + 'solMonth=' + solMonth + '&' + 'ServiceKey=' + SERVICEKEY + '&' + '_type=json';
+  
+  return new Promise((resolve, reject) => {  
+    request(requestUrl, function (error, response, body) {
+      if (error) {
+        reject ({ status: response.statusCode, message: "openApi query error" });
+      }
+      const returnData = JSON.parse(body);
+      const items = returnData['response']['body']['items']['item'];
+      let holidayArray = [];
+      if (Array.isArray(items)) {
+        for (let i = 0; i < items.length; i++) {
+          let item = items[i];
+          if (item['locdate']) {
+            holidayArray.push(item['locdate']);
+          }
+        }
+      } else if (typeof items === 'object') {
+        if (items['locdate']) {
+          holidayArray.push(items['locdate']);
+        }
+      }
+      resolve ({ status: response.statusCode, data: holidayArray });
+    });
+  });
 }
