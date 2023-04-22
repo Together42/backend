2a10dd4ded540f7cf8ed70c082ba6efb3709aa36
Jiwon-Woo <jwoo@student.42seoul.kr>
2023-04-22 20:24:27 +0900

    feat: S3 이전

--- src/s3.js
+++ src/s3.js
@@ -4,5 +4,6 @@ import { config } from "./config.js";
 export const s3 = new aws.S3({
   accessKeyId: config.s3.access_key_id,
   secretAccessKey: config.s3.secret_access_key,
+  bucket: config.s3.bucket,
   region: config.s3.region,
 });
