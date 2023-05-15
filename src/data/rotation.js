38f31761bd1e96e09af55446f54a241149e19721
sideseal <ckgun@naver.com>
2023-05-15 16:57:59 +0900

    fix: fix query holiday

--- src/data/rotation.js
+++ src/data/rotation.js
@@ -94,3 +94,37 @@ export async function updateAttendDate(attendInfo) {
     )
     .then((result) => result[0]);
 }
+
+export async function addHolidayInfo(holidayInfo) {
+  const { year, month, day, info } = holidayInfo;
+  try {
+    const [rows] = await db.execute(
+      "SELECT * FROM holiday_info WHERE year = ? AND month = ? AND day = ?", 
+      [year, month, day],
+    );
+    if (rows.length === 0) {
+      return db
+        .execute("INSERT INTO holiday_info (month, year, day, info) VALUES (?,?,?,?)",
+        [month, year, day, info],
+        )
+        .then((result) => result[0]);
+    } else {
+      console.log('Record already exists');
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getHolidayByMonth(holidayInfo) {
+  const { year, month } = holidayInfo;
+  try {
+    return db
+      .execute("SELECT * FROM holiday_info WHERE year = ? AND month = ?", 
+      [year, month],
+      )
+      .then((result) => result[0]);
+  } catch (error) {
+    throw error;
+  }
+}
--- src/utils/rotation.calendar.js
+++ src/utils/rotation.calendar.js
@@ -1,6 +1,6 @@
+import { addHolidayInfo, getHolidayByMonth } from "../data/rotation.js";
 import request from "request";
 import { config } from "../config.js";
-
 
 export function getTodayDate() {
   const today = new Date();
@@ -28,35 +28,64 @@ export function getFourthWeekdaysOfMonth() {
   return (fourthWeekDays);
 }
 
-export async function getHolidayOfMonth() {
+export async function storeHolidayInfo() {
   const URL = 'http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getHoliDeInfo';
-  const solYear = new Date().getFullYear();
-  const month = new Date().getMonth() + 1;
+  let month = new Date().getMonth() + 2;
   const solMonth = month < 10 ? '0' + month : month;
+  let year = new Date().getFullYear();
+  const solYear = month === 1 ? year += 1 : year;
   const SERVICEKEY = config.openApi.holidayKey;
   const requestUrl = URL + '?' + 'solYear=' + solYear + '&' + 'solMonth=' + solMonth + '&' + 'ServiceKey=' + SERVICEKEY + '&' + '_type=json';
-  
-  return new Promise((resolve, reject) => {  
-    request(requestUrl, function (error, response, body) {
+
+  return new Promise((resolve, reject) => {
+    request(requestUrl, async function (error, response, body) {
       if (error) {
         reject ({ status: response.statusCode, message: "openApi query error" });
       }
       const returnData = JSON.parse(body);
       const items = returnData['response']['body']['items']['item'];
-      let holidayArray = [];
       if (Array.isArray(items)) {
         for (let i = 0; i < items.length; i++) {
+          let holidayArray = [];
           let item = items[i];
           if (item['locdate']) {
-            holidayArray.push(item['locdate']);
+            console.log(item['locdate'], typeof(item['locdate']));
+            const holidayInfo = {
+              year: item['locdate'].toString().substr(0, 4),
+              month: item['locdate'].toString().substr(4, 2),
+              day: item['locdate'].toString().substr(6, 2),
+              info: item['dateName']
+            };
+            holidayArray.push(holidayInfo);
+
+            await addHolidayInfo(holidayInfo)
+              .catch(error => reject({ status: 500, message: error }));
           }
         }
       } else if (typeof items === 'object') {
         if (items['locdate']) {
-          holidayArray.push(items['locdate']);
+          let holidayArray = [];
+          const holidayInfo = {
+            year: items['locdate'].toString().substr(0, 4),
+            month: items['locdate'].toString().substr(4, 2),
+            day: items['locdate'].toString().substr(6, 2),
+            info: items['dateName']
+          };
+          holidayArray.push(holidayInfo);
+
+          await addHolidayInfo(holidayInfo)
+            .catch(error => reject({ status: 500, message: error }));
         }
       }
-      resolve ({ status: response.statusCode, data: holidayArray });
+      resolve ({ status: response.statusCode });
     });
   });
+}
+
+export async function getHolidayOfMonth() {
+  let month = new Date().getMonth() + 2;
+  let year = month === 1 ? new Date().getFullYear() + 1 : new Date().getFullYear();
+  month = 11;
+  const holidayArray = await getHolidayByMonth({ year: year, month: month });
+  console.log(holidayArray);
 }
